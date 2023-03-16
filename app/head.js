23d40c03dcc5d6c365b4ba2be9e37dd10209e93f
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-16 16:23:04 +0600

    working on navbar

--- app/head.js
+++ app/head.js
@@ -2,7 +2,7 @@ const Head = () => (
 	<>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="icon" href="/favicon.ico" />
-		<title>Metaverse</title>
+		<title>Metaverse - Madness</title>
 	</>
 );
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,25 @@
 "use client";
-
-const Navbar = () => <nav>navbar</nav>;
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { navVariants } from "../utils/motion";
+const Navbar = () => (
+	<motion.nav
+		variants={navVariants}
+		initial="hidden"
+		whileInView="show"
+		className="sm:px-16 px-6 py-8 relative">
+		<div className="absolute w-[50%] inset-0 gradient-01" />
+		<div className="2xl:max-w-[1280px] w-full mx-auto flex justify-between gap-8">
+			<Image
+				src="/search.svg"
+				alt="search"
+				width={24}
+				height={24}
+				className="object-contain"
+			/>
+			<h2>METAVERSE MADNESS</h2>
+		</div>
+	</motion.nav>
+);
 
 export default Navbar;
