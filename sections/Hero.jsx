dec2a7d7296eb05acecbec2cd0fbef0539125d4f
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-20 04:23:56 +0600

    adding cover image on hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
+import Image from "next/image";
 
 import styles from "../styles";
-import { staggerContainer, textVariant } from "../utils/motion";
+import {slideIn, staggerContainer, textVariant} from "../utils/motion";
 
 const Hero = () => (
 	<section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
@@ -27,6 +28,19 @@ const Hero = () => (
 					<h1 className={styles.heroHeading}>Ness</h1>
 				</motion.div>
 			</div>
+
+			<motion.div
+				variants={slideIn("right", "tween", 0.2, 1)}
+				className="relative w-full md:-mt-[20px] -mt-[12px]">
+				<div className="absolute w-full h-[300px] hero-gradient rounded-top-[140px] z-[0] -top-[30px]">
+					<Image
+						src="/cover.png"
+						alt="cover"
+						fill
+						className=" sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+					/>
+				</div>
+			</motion.div>
 		</motion.div>
 	</section>
 );
