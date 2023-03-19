9b6b776db6bf7a458dddcc39e2d3ae308d84be96
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-20 04:25:45 +0600

    typo fix on styling

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 import styles from "../styles";
-import {slideIn, staggerContainer, textVariant} from "../utils/motion";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
 	<section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
@@ -32,7 +32,7 @@ const Hero = () => (
 			<motion.div
 				variants={slideIn("right", "tween", 0.2, 1)}
 				className="relative w-full md:-mt-[20px] -mt-[12px]">
-				<div className="absolute w-full h-[300px] hero-gradient rounded-top-[140px] z-[0] -top-[30px]">
+				<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]">
 					<Image
 						src="/cover.png"
 						alt="cover"
