11dea55a76e8a59082e5f81243d5f7e9b0f1fb5a
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-22 03:03:49 +0600

    hero section complete

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,19 +1,18 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
-import Image from "next/image";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
-	<section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+	<section className="sm:py-16 xs:py-8 py-12 sm:pl-16 pl-6">
 		<motion.div
 			variants={staggerContainer}
 			initial="hidden"
 			whileInView="show"
 			viewport={{ once: false, amount: 0.25 }}
-			className="2xl:max-w-[1280px] w-full mx-auto flex flex-col">
+			className="lg:w-[80%] w-[100%] mx-auto flex flex-col">
 			<div className="flex justify-center items-center flex-col relative z-10">
 				<motion.h1
 					variants={textVariant(1.1)}
@@ -30,26 +29,25 @@ const Hero = () => (
 			</div>
 
 			<motion.div
-				variants={slideIn("right", "tween", 0.2, 1)}
+				variants={slideIn('right', 'tween', 0.2, 1)}
 				className="relative w-full md:-mt-[20px] -mt-[12px]">
-				<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]">
-					<Image
-						src="/cover.png"
-						alt="cover"
-						fill
-						className=" sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
-					/>
-					<a href="#explore">
-						<div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10 ">
-							<Image
-								src="/stamp.png"
-								alt="stamp"
-								fill
-								className="sm:w-[155px] w-[100px]"
-							/>
-						</div>
-					</a>
-				</div>
+				<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+
+				<img
+					src="/cover.png"
+					alt="hero_cover"
+					className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+				/>
+
+				<a href="#explore">
+					<div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+						<img
+							src="/stamp.png"
+							alt="stamp"
+							className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+						/>
+					</div>
+				</a>
 			</motion.div>
 		</motion.div>
 	</section>
