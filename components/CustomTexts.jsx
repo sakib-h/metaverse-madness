c155fc10593647bd43681d5e4efd7723e2356dd4
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-22 03:30:57 +0600

    some previous fumction solved

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,15 @@
 'use client';
+import { motion } from 'framer-motion';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { textContainer } from '../utils/motion';
+export const TypingText = ({ title, textStyles }) => (
+	<motion.p
+		variants={textContainer}
+		className={`font-normal text*[14px] text-secondary-white ${textStyles}}`}>
+		{Array.from(title).map((letter, index) => (
+			<motion.span key={index}></motion.span>
+		))}
+	</motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = () => <h2>Title Text</h2>;
--- sections/About.jsx
+++ sections/About.jsx
@@ -1,14 +1,20 @@
-"use client";
+'use client';
 
 import { motion } from 'framer-motion';
-import {TypingText} from "../components"
-import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
-const About = () => <section className="sm:p-16 xs:p-8 px-6 py-12 relative z-10"  >
-    <div className="gradient-01 z-0"/>
-  <motion.div variants={staggerContainer} initial="hidden" whileInView="show" vireport={{once:false, amount:0.25}} className="lg:w-[80%] w-[100%] mx-auto flex justify-center items-center flex-col">
-<TypingText title="| About Metaversus" textStyles = "text-center"/>
-
-    </motion.div></section>;
+import { TypingText } from '../components';
+import { staggerContainer } from '../utils/motion';
+const About = () => (
+	<section className="sm:p-16 xs:p-8 px-6 py-12 relative z-10">
+		<div className="gradient-01 z-0" />
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView="show"
+			vireport={{ once: false, amount: 0.25 }}
+			className="lg:w-[80%] w-[100%] mx-auto flex justify-center items-center flex-col">
+			<TypingText title="| About Metaversus" textStyles="text-center" />
+		</motion.div>
+	</section>
+);
 
 export default About;
