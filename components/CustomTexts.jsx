180f4f6ee6d79ca91d8d417888aa1f92293ce4a4
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-22 15:04:54 +0600

    working on explore card component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -7,11 +7,21 @@ export const TypingText = ({ title, textStyles }) => (
 		variants={textContainer}
 		className={`font-normal text*[14px] text-secondary-white ${textStyles}}`}>
 		{Array.from(title).map((letter, index) => (
-			<motion.span key={index} variants={textVariant2}>
+			<motion.span
+				key={index}
+				variants={textVariant2}>
 				{letter === ' ' ? '\u00A0' : letter}
 			</motion.span>
 		))}
 	</motion.p>
 );
 
-export const TitleText = () => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+	<motion.h2
+		variants={textVariant2}
+		initial="hidden"
+		whileInView="show"
+		className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}>
+		{title}
+	</motion.h2>
+);
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,33 +1,47 @@
 'use client';
 import { motion } from 'framer-motion';
-import { TitleText, TypingText } from '../components';
+import { useState } from 'react';
+import { ExploreCard, TitleText, TypingText } from '../components';
+import { exploreWorlds } from '../constants';
 import { staggerContainer } from '../utils/motion';
-
-const Explore = () => (
-	<section
-		className="sm:p-16 xs:p-8 px-6 py-12"
-		id="explore">
-		<motion.div
-			variants={staggerContainer}
-			initial="hidden"
-			whileInView="show"
-			viewport={{ once: false, amount: 0.5 }}
-			className="2xl:max-w-[1280px] w-full mx-auto flex flex-col">
-			<TypingText
-				title="| The World"
-				textStyles="text-center"
-			/>
-			<TitleText
-				title={
-					<>
-						Choose the World you want{' '}
-						<br className="md:block hidden" /> to explore
-					</>
-				}
-				textStyles="text-center"
-			/>
-		</motion.div>
-	</section>
-);
+const Explore = () => {
+	const [active, setActive] = useState('world-2');
+	return (
+		<section
+			className="sm:p-16 xs:p-8 px-6 py-12"
+			id="explore">
+			<motion.div
+				variants={staggerContainer}
+				initial="hidden"
+				whileInView="show"
+				viewport={{ once: false, amount: 0.5 }}
+				className="2xl:max-w-[1280px] w-full mx-auto flex flex-col">
+				<TypingText
+					title="| The World"
+					textStyles="text-center"
+				/>
+				<TitleText
+					title={
+						<>
+							Choose the World you want{' '}
+							<br className="md:block hidden" /> to explore
+						</>
+					}
+					textStyles="text-center"
+				/>
+				<div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+					{exploreWorlds.map((world, index) => (
+						<ExploreCard
+							key={world.id}
+							{...world}
+							active={active}
+							handleClick={setActive}
+						/>
+					))}
+				</div>
+			</motion.div>
+		</section>
+	);
+};
 
 export default Explore;
