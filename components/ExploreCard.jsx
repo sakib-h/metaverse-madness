9ee0b741e7c78fee6ef60899ca7b90d2ea3a8f19
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-25 19:51:30 +0600

    Adding title on explore card

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -31,6 +31,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
 				<p className="font-normal text-[16px] leading-[20px] text-white uppercase">
 					Enter the Metaverse
 				</p>
+				<h2 className="mt-[24px] font-semibold sm:text-[32px]"><{title}/h2>
 			</div>
 		)}
 	</motion.div>
