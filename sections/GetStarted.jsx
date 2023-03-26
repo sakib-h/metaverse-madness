7127d4e6c204861443914dec5f79e4dff579065d
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-27 04:00:22 +0600

    working on getStarted Sections

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,5 +1,8 @@
 'use client';
 import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { startingFeatures } from '../constants';
 const GetStarted = () => <section>Get Started Section</section>;
 
 export default GetStarted;
