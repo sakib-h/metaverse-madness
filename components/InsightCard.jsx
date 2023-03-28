68441acb3d5aa4955cc096fe0fc49b2ddc3e3a30
Sakib Hasan <sakib100.sa@gmail.com>
2023-03-29 02:12:24 +0600

    working on instight card component

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,21 @@
 'use client';
-
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+	<motion.div
+		variant={fadeIn('up', 'spring', index * 0.5, 1)}
+		className="flex md:flex-row flex-col gap-4 ">
+		<img
+			src={imgUrl}
+			className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+		/>
+    <div className="w-full flex justify-between items-center ">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px] ">
+        <h4>{title}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+	</motion.div>
 );
 
 export default InsightCard;
--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,9 +1,35 @@
 'use client';
-
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
+import { insights } from '../constants';
 const Insights = () => (
-  <section>
-    Insights section
-  </section>
+	<section className="sm:p-16 xs:p-8 px-6 py-12 relative z-10">
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: 'false', amount: 0.25 }}
+			className="2xl:max-w-[1280px] w-full mx-auto flex flex-col items-center">
+			<TypingText
+				title="| Insight"
+				textStyles="text-center"
+			/>
+			<TitleText
+				title="Insight about metaverse"
+				textStyles="text-center"
+			/>
+			<div className="mt-[50px] flex flex-col gap-[30px]">
+				{insights.map((insight, index) => (
+					<InsightCard
+						key={`insight-${index}`}
+						index={index + 1}
+						{...insight}
+					/>
+				))}
+			</div>
+		</motion.div>
+	</section>
 );
 
 export default Insights;
