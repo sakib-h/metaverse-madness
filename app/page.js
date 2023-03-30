7e2fadfcfb34253d43229bedd554ea47b8b251ec
sakib-h <sakib100.sa@gmail.com>
2023-03-30 15:01:04 +0600

    working on commit

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import {
 } from '../sections';
 
 const Page = () => (
-	<div className="footer bg-primary-black overflow-hidden">
+	<div className="bg-primary-black overflow-hidden">
 		<Navbar />
 		<Hero />
 		<div className="relative">
